8230d5bff36a3f6eb84a56dd6a0693147a60944d
PHET45 <phnjk2000@gmail.com>
2025-07-18 12:21:57 +0700

    feat: implement light bulb status messages in ex-2.js

--- ex-2.js
+++ ex-2.js
@@ -1,4 +1,13 @@
 //Exercise 2
 let lightBulbStatus = "On";
+if(lightBulbStatus === "On"){
+    console.log("Light bulb is On.");
+} else if (lightBulbStatus === "Off"){
+    console.log("Light bulb is Off.");
+} else if(lightBulbStatus === "Broken"){
+    console.log("Light bulb is Broken.");
+} else {
+    console.log("Please choose the correct input (On/Off/Broken)");
+};
 
 // Start coding here.
