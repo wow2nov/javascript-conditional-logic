260db385cdb37618dbd2286ce710a51d85caec0d
PHET45 <phnjk2000@gmail.com>
2025-07-18 13:18:04 +0700

    refactor: update greeting implementation in ex-7.js to log message instead of calling as a function

--- ex-7.js
+++ ex-7.js
@@ -1,6 +1,3 @@
-//Start coding here
-const greeting = "Hello ! 😁";
-
-greeting();
-
+const greeting = "Hello ! 😁"
+console.log(greeting);
 console.log("This line will be executed without any error.");
