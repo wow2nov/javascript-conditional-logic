70d27fa68f0bd6f5150f442b20fe37e837d7610e
Supapond <supapondmaliwab@gmail.com>
2025-07-18 13:54:18 +0700

    refactor: improve in ex-5.js and ex-6.js

--- ex-5.js
+++ ex-5.js
@@ -1,23 +1,17 @@
 function calculateStudentGrade(score) {
   // Start coding here
- if (score >=90 && score <=100) {
+  if (score >= 90 && score <= 100) {
     return "A";
-} else if (score >= 80 && score <=89) {
+  } else if (score >= 80 && score <= 89) {
     return "B";
-} else if (score >= 70 && score <=79) {
+  } else if (score >= 70 && score <= 79) {
     return "C";
-} else if (score >= 60 && score <=69) {
+  } else if (score >= 60 && score <= 69) {
     return "D";
-} else if (score >= 0 && score <=59) {
-  return "F";    
-} else {
-    return "Invalid score"; 
+  } else if (score >= 0 && score <= 59) {
+    return "F";
+  }
 }
-
-}
-
-
-
 
 // Example case
 let johnGrade = calculateStudentGrade(30);
--- ex-6.js
+++ ex-6.js
@@ -2,13 +2,18 @@ let favoriteBooks = [];
 
 function addFavoriteBook(bookName) {
   // Start coding here !
+
+  if (bookName.includes("Beauty")) {
+    favoriteBooks.push(bookName);
+  }
 }
 
 // Example case
-addFavoriteBook("Let Me Tell You What I Mean");
+addFavoriteBook("Let Me Tell You What I Mean Beauty");
 addFavoriteBook("Life Among the Terranauts");
 addFavoriteBook("The Beauty of Living Twice");
 addFavoriteBook("Black Beauty");
 addFavoriteBook("Monstrous Beauty");
 
 console.log(favoriteBooks);
+
