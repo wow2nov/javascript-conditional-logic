8c8fdb357512e576fea33781bad1665b4bb7bf0a
Supapond <supapondmaliwab@gmail.com>
2025-07-18 12:37:10 +0700

    feat: add light bulb status check to ex-1.js

--- ex-1.js
+++ ex-1.js
@@ -2,3 +2,9 @@
 let lightBulbStatus = "On";
 
 // Start coding here.
+if (lightBulbStatus === "On") {
+    console.log ("Light bulb is on.");
+} else if (lightBulbStatus === "Off") {
+    console.log ("Light bulb is off.");
+}
+
