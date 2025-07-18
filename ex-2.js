9cd44ff54d047cb0e6526737876ab7aa81c9af61
Supapond <supapondmaliwab@gmail.com>
2025-07-18 12:45:58 +0700

    feat: update in ex-2

--- ex-2.js
+++ ex-2.js
@@ -1,4 +1,11 @@
 //Exercise 2
 let lightBulbStatus = "On";
+lightBulbStatus = "Broken";
 
 // Start coding here.
+if (lightBulbStatus === "Broken") {
+    console.log("Light bulb is Broken.");
+}
+ else  {
+    console.log ("Please choose the correct input (On/Off/Broken)");
+}
